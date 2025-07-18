0c1a670178149472ae979474c9b9a59b48375a84
Nanuzer0 <ivragov3@gmail.com>
2025-07-18 21:29:48 +0300

    upd hook 4 jonkler

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -1,7 +1,8 @@
 local sc = Card.set_cost
 function Card:set_cost()
     sc(self)
-    if (self.config.center.rarity == "nanu_dogsh") then
+    -- this works fine for one joker but if i add more this needs rework
+    if (self.config.center.rarity == "nanu_dogsh" and self.config.center_key ~= 'j_nanu_wouldyourather') then
         self.cost = 0
     end
 end
